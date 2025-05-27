dadea82fd66363eda04066b4949b5bd535f089d5
seongwoo <mhs4670go@naver.com>
2025-05-27 16:06:02 +0900

    apply comments.

--- test/unit_test/pass_test/test_decompose_fake_quantize.py
+++ test/unit_test/pass_test/test_decompose_fake_quantize.py
@@ -13,14 +13,10 @@
 # limitations under the License.
 
 import torch
-from tico.experimental.quantization.passes.remove_weight_dequant_op import (
-    RemoveWeightDequantOp,
-)
-from tico.passes.const_prop_pass import ConstPropPass
 from tico.passes.decompose_fake_quantize import DecomposeFakeQuantize
 
 from test.utils.helper import num_of_ops
-from test.utils.pass_value_test import PassTest, SinglePassValueTest
+from test.utils.pass_value_test import SinglePassValueTest
 
 
 class FakeQuantizePerChannel(torch.nn.Module):
