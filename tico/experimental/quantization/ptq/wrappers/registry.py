007f99d62767a2fca7c5224fb777c4244e17392f
seongwoo <mhs4670go@naver.com>
2025-08-08 11:02:59 +0900

    [DRAFT] Introduce PTQWrapper
    This draft introduces PTQWrapper.
    
    TICO-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- tico/experimental/quantization/ptq/wrappers/registry.py
+++ tico/experimental/quantization/ptq/wrappers/registry.py
@@ -31,6 +31,11 @@ _CORE_MODULES = (
     "tico.experimental.quantization.ptq.wrappers.llama.quant_attn",
     "tico.experimental.quantization.ptq.wrappers.llama.quant_decoder_layer",
     "tico.experimental.quantization.ptq.wrappers.llama.quant_mlp",
+    "tico.experimental.quantization.ptq.wrappers.fairseq.quant_mha",
+    "tico.experimental.quantization.ptq.wrappers.fairseq.quant_encoder_layer",
+    "tico.experimental.quantization.ptq.wrappers.fairseq.quant_encoder",
+    "tico.experimental.quantization.ptq.wrappers.fairseq.quant_decoder_layer",
+    "tico.experimental.quantization.ptq.wrappers.fairseq.quant_decoder",
     # add future core wrappers here
 )
 
