2bbb5364e7d7570b09448a551feb1faed799d692
seockho.kim <seockho.kim@samsung.com>
2025-09-12 12:49:51 +0900

    [test] Formatting model.py
    This commit fixes formatting of model.py
    
    TICO-DCO-1.0-Signed-off-by: Seockho Kim seockho.kim@samsung.com

--- test/modules/model/Gemma3/model.py
+++ test/modules/model/Gemma3/model.py
@@ -21,21 +21,24 @@ from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS
 from test.modules.base import TestModuleBase
 from test.utils import tag
 
+
 @tag.use_onert
 class Gemma3(TestModuleBase):
     def __init__(self):
         super().__init__()
 
         ckpt = "google/gemma-3-270m"
-        
+
         self.model = Gemma3ForCausalLM.from_pretrained(
-            ckpt,
-            torch_dtype=torch.float32,
-            device_map="cpu"
+            ckpt, torch_dtype=torch.float32, device_map="cpu"
         )
         # This is the same as sdpa, but mask creation does not use `vmap` which is not exportable
-        ALL_MASK_ATTENTION_FUNCTIONS.register("sdpa_without_vmap", sdpa_mask_without_vmap)
-        ALL_ATTENTION_FUNCTIONS.register("sdpa_without_vmap", ALL_ATTENTION_FUNCTIONS["sdpa"])
+        ALL_MASK_ATTENTION_FUNCTIONS.register(
+            "sdpa_without_vmap", sdpa_mask_without_vmap
+        )
+        ALL_ATTENTION_FUNCTIONS.register(
+            "sdpa_without_vmap", ALL_ATTENTION_FUNCTIONS["sdpa"]
+        )
         self.model.config._attn_implementation = "sdpa_without_vmap"
 
         self.rtol = 1e-3
@@ -47,12 +50,11 @@ class Gemma3(TestModuleBase):
     def get_example_inputs(self):
         input_ids = torch.randint(0, 100000, (1, 7), dtype=torch.int64)
         attention_mask = torch.ones((1, 7), dtype=torch.int64)
-        
+
         return (
             (input_ids,),
             {
-                "attention_mask":attention_mask,
-                "use_cache":False,
+                "attention_mask": attention_mask,
+                "use_cache": False,
             },
         )
-       
