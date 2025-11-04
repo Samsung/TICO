9efa26fb3bdb7f7b35515e3db3e04a759b684c38
Sanggyu Lee <sg5.lee@samsung.com>
2025-11-04 14:11:16 +0900

    Remove remove_unused_inputs pass

--- tico/utils/convert.py
+++ tico/utils/convert.py
@@ -58,7 +58,6 @@ from tico.passes.remove_redundant_permute import passes as RemoveRedundantPermut
 from tico.passes.remove_redundant_reshape import passes as RemoveRedundantViewPasses
 from tico.passes.remove_redundant_slice import RemoveRedundantSlice
 from tico.passes.remove_redundant_to_copy import RemoveRedundantToCopy
-from tico.passes.remove_unused_inputs import RemoveUnusedInput
 from tico.passes.restore_linear import RestoreLinear
 from tico.passes.segment_index_select import SegmentIndexSelectConst
 from tico.quantization.passes.fold_quant_ops import FoldQuantOps
@@ -262,7 +261,6 @@ def convert_exported_module_to_circle(
             *LowerToSlicePasses(),
             FuseLeadingUnsqueezeReshape(),
             CastClampMixedTypeArgs(),
-            RemoveUnusedInput(),
         ]
     )
     circle_legalize.run(exported_program)
