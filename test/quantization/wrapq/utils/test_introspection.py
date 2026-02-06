817ddc2fd0182adac76747e02c3d88a0f0b22485
seongwoo chae <mhs4670go@naver.com>
2026-02-06 11:01:23 +0900

    Update test/quantization/wrapq/utils/test_introspection.py
    Co-authored-by: Seungho Henry Park <shs.park@samsung.com>

--- test/quantization/wrapq/utils/test_introspection.py
+++ test/quantization/wrapq/utils/test_introspection.py
@@ -122,7 +122,7 @@ class TestSmoothQuantPTQDiff(unittest.TestCase):
                 sq_model(ids)
         sq_model = convert(sq_model, inplace=True)
 
-        # PTQ-wrap first layers
+        # PTQ-wrap first layer
         qcfg = PTQConfig()
         new_layers = torch.nn.ModuleList()
         for idx, fp_layer in enumerate(sq_model.model.layers):
