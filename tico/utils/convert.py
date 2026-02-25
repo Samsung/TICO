72801c0533fb90d10c58183bb181c66a8352827e
Seok Namkoong <seok9311@naver.com>
2026-02-25 17:11:37 +0900

    [passes] Introduce ConvertPermuteToReshape pass
    This commit introduces `ConvertPermuteToReshape` pass.
    
    TICO-DCO-1.0-TICO-DCO-1.0-Signed-off-by: Seok Namkoong <seok9311@naver.com>

--- tico/utils/convert.py
+++ tico/utils/convert.py
@@ -29,6 +29,7 @@ from tico.passes.convert_conv3d_to_conv2d import ConvertConv3dToConv2d
 from tico.passes.convert_expand_to_slice_cat import ConvertExpandToSliceCat
 from tico.passes.convert_layout_op_to_reshape import ConvertLayoutOpToReshape
 from tico.passes.convert_matmul_to_linear import ConvertMatmulToLinear
+from tico.passes.convert_permute_to_reshape import ConvertPermuteToReshape
 from tico.passes.convert_repeat_to_expand_copy import ConvertRepeatToExpandCopy
 from tico.passes.convert_sym_size_to_circle_shape import ConvertSymSizeToCircleShape
 from tico.passes.convert_to_relu6 import ConvertToReLU6
@@ -273,6 +274,7 @@ def convert_exported_module_to_circle(
             *LowerToSlicePasses(),
             FuseLeadingUnsqueezeReshape(),
             CastClampMixedTypeArgs(),
+            ConvertPermuteToReshape(),
         ]
     )
     circle_legalize.run(exported_program)
