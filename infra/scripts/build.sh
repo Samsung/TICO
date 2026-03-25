6ce99430f8d17949abb25c79b01e42db45f465e5
seongwoo <mhs4670go@naver.com>
2026-03-25 12:42:34 +0900

    [infra] Upgrade pip version when bulding
    This commit upgrades pip version when building.
    
    TICO-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- infra/scripts/build.sh
+++ infra/scripts/build.sh
@@ -2,7 +2,16 @@
 
 # Install Required Package
 #
-# NOTE To add additional build dependencies, append to `requires` field of [build-system] in pyproject.toml 
+# NOTE To add additional build dependencies, append to `requires` field of [build-system] in pyproject.toml
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+###############################################################################
+# Install the auxiliary Python requirements
+###############################################################################
+REQ_FILE="${SCRIPTS_DIR}/build_requirements.txt"
+echo "[INFO] Installing auxiliary requirements from ${REQ_FILE##*/}"
+python3 -m pip install -r "$REQ_FILE"
 python3 -m pip install build
 
 # Build
