7db5d395cd441005b25c94f56dc6c2f603f10afa
seongwoo chae <mhs4670go@naver.com>
2026-08-22 01:47:26 +0900

    [examples] Conditional weight sensitivity (#944)
    This commit supports conditonal weight sensitivity.
    
    TICO-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- examples/hand_detector/analyze.py
+++ examples/hand_detector/analyze.py
@@ -24,6 +24,7 @@ from typing import Any
 from examples.hand_detector import (
     adaround as adaround_cli,
     block_reconstruction as block_reconstruction_cli,
+    conditional_weight_sensitivity as conditional_weight_sensitivity_cli,
     precision_ablation as precision_ablation_cli,
     reverse_weight_precision as reverse_weight_precision_cli,
     weight_family_ablation as weight_family_ablation_cli,
@@ -224,6 +225,7 @@ def parse_args() -> argparse.Namespace:
     )
 
     block_reconstruction_cli.add_subparser(subparsers)
+    conditional_weight_sensitivity_cli.add_subparser(subparsers)
     adaround_cli.add_subparser(subparsers)
     precision_ablation_cli.add_subparser(subparsers)
     reverse_weight_precision_cli.add_subparser(subparsers)
@@ -348,6 +350,8 @@ def main() -> None:
         _run_group_observer_sweep(args)
     elif args.command == "block-reconstruction":
         block_reconstruction_cli.run(args)
+    elif args.command == "conditional-weight-sensitivity":
+        conditional_weight_sensitivity_cli.run(args)
     elif args.command == "adaround":
         adaround_cli.run(args)
     elif args.command == "precision-ablation":
